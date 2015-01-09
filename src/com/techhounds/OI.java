6a553cbbe545758d7de039ea369ed32931f0a312
Ayon2 <Ayon2@10.33.39.71>
2015-01-09 16:53:29 -0500

    deleted Atif's comment

--- src/com/techhounds/OI.java
+++ src/com/techhounds/OI.java
@@ -50,39 +50,6 @@ public class OI {
     
     public void initDriver() {
     	
-    	/* Hi Alex, Matt, Clayton and Ayon,
-    	 * I want to introduce you to the new way of calling Joystick buttons.
-    	 * Here is the current way you did it.
-    	 * 
-    	 * Button liftUp = new JoystickButton(gamepad, LIFT_UP);
-    	 * liftUp.whenPressed(new MoveLift());
-    	 * 
-    	 * This isnt going to work for us today. Instead we got this new awesome 
-    	 * class called the ControllerMap. It is in com.techhounds if you want to
-    	 * see it. With this class, it contains a unique library which allows us to
-    	 * use more than one type of controller such as Logtiech or PS3.
-    	 * 
-    	 * Look at the constructor for OI. If you notice, there are two ControllerMaps.
-    	 * One is for teh driver, one is for the operator. 
-    	 * 
-    	 * So up above I told you that you called that button weird right? Well guess what?
-    	 * Ill tell you how to call it with out new code.
-    	 * So here we go!
-    	 * 
-    	 * Button liftUp = driver.createButton(ControllerMap.A);
-    	 * liftUp.whenPressed(new MoveLift(LiftSubsystem.Direction.UP));
-    	 * 
-    	 * The parameter in driver.createButton() is ControllerMap.A. ControllerMap.A is
-    	 * the button A on the controller, so basically, you dont have to fiddle with ports!
-    	 * 
-    	 * Other from that it works like a normal button. Good Work so far. Proud of what
-    	 * you have done so far! Keep up the good work!
-    	 * 
-    	 * Please delete this comment since it is using so many lines. Lol.
-    	 * 
-    	 * ~~ Atif
-    	 */
-    	
     	Button liftUp = driver.createButton(ControllerMap.A);
     	liftUp.whenPressed(new RunLift());
     	
