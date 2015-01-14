675ccd083891d56dfaa7bb90d7b9b12642746202
Ayon2 <Ayon2@10.35.84.134>
2015-01-14 15:40:15 -0500

    Created Compressor Subsystem

---
+++ src/com/techhounds/subsystems/CompSubsystem.java
@@ -0,0 +1,13 @@
+package com.techhounds.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+
+public class CompSubsystem extends Subsystem {
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
