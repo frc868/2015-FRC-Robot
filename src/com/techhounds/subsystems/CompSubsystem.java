5232c0370cbd0f71b80706273f87ae20f943fc7e
Ayon2 <Ayon2@10.35.84.134>
2015-01-14 15:49:12 -0500

    Controller Map was updated to new requirements

--- src/com/techhounds/subsystems/CompSubsystem.java
+++ src/com/techhounds/subsystems/CompSubsystem.java
@@ -1,13 +1,23 @@
 package com.techhounds.subsystems;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 
 public class CompSubsystem extends Subsystem {
+	
+	private static CompSubsystem instance;
+	
+	private Compressor comp;
+	
+	public static CompSubsystem getInstance() {
+		if (instance == null) {
+			
+		}
+	}
 
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+       
     }
 }
 
