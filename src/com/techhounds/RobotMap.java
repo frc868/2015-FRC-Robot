799552f3de6a0afbbccdde06cf080f1e69853850
Ayon2 <Ayon2@10.35.84.134>
2015-01-15 20:02:21 -0500

    made some changes to robot map

--- src/com/techhounds/RobotMap.java
+++ src/com/techhounds/RobotMap.java
@@ -2,29 +2,31 @@ package com.techhounds;
 
 public class RobotMap {
 	
+	public static final int DOES_NOT_EXIST = 0;
+	
 	public static class Drive{
-		public static final int DRIVER_PORT = 1;
-		public static final int OPERATOR_PORT = 2;
-		public static final int RIGHT_DRIVE_MOTOR_1 = 0;
-		public static final int RIGHT_DRIVE_MOTOR_2 = 0;
-		public static final int RIGHT_DRIVE_MOTOR_3 = 0;
-		public static final int LEFT_DRIVE_MOTOR_1 = 0;
-		public static final int LEFT_DRIVE_MOTOR_2 = 0;
-		public static final int LEFT_DRIVE_MOTOR_3 = 0;
+		public static final int DRIVER_PORT = 0;
+		public static final int OPERATOR_PORT = 1;
+		
+		public static final int RIGHT_DRIVE_MOTOR_1 = 19;
+		public static final int RIGHT_DRIVE_MOTOR_2 = 18;
+		public static final int RIGHT_DRIVE_MOTOR_3 = 17;
+		public static final int LEFT_DRIVE_MOTOR_1 = 16;
+		public static final int LEFT_DRIVE_MOTOR_2 = 15;
+		public static final int LEFT_DRIVE_MOTOR_3 = 14;
 	}
 	
 	public static class Lift{
-		public static final int LIFT_MOTOR = 0;
+		public static final int LIFT_MOTOR = 9;
 		public static final int DIGITAL_INPUT_TOP = 0;
-		public static final int DIGITAL_INPUT_BOTTOM = 0;
-		public static final int LIFT_POWER = 1;
+		public static final int DIGITAL_INPUT_BOTTOM = 1;
 		public static final int LIFT_SOL = 0;
 	}
 	
 	public static class Arms{
-		public static final int LEFT_ARM = 0;
-		public static final int RIGHT_ARM = 1;
-		public static final int ARM_SOL = 0;
+		public static final int LEFT_ARM = DOES_NOT_EXIST;//8
+		public static final int RIGHT_ARM = DOES_NOT_EXIST;//7
+		public static final int ARM_SOL = DOES_NOT_EXIST;//3
 	}
 	
 	public static class Compressor {
