dc475da6f7b1c7f2fb920da8073eb9b1a1e16e59
alexanderfleig <alex@fleigs.com>
2015-01-20 19:21:32 -0500

    Added Elevator commands

---
+++ src/com/techhounds/commands/ElevateUp.java
@@ -0,0 +1,45 @@
+
+package com.techhounds.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import com.techhounds.subsystems.*;
+/**
+ *
+ */
+public class ElevateUp extends Command {
+
+	private ElevatorSubsystem elevate;
+	
+    public ElevateUp() {
+    	elevate = ElevatorSubsystem.getInstance();
+        requires(elevate);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	elevate.startRaisingElevator();
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	return !elevate.getTopSwitch();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	elevate.stopElevator();
+    	elevate.setLed(ElevatorSubsystem.ON);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
