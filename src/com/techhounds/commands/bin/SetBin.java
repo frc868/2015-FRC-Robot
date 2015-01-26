ab883a7a4551e0bcb4d4102e3997826f1fe38e05
alexanderfleig <alex@fleigs.com>
2015-01-26 17:28:28 -0500

    Worked on SetBin command

--- src/com/techhounds/commands/bin/SetBin.java
+++ src/com/techhounds/commands/bin/SetBin.java
@@ -1,19 +1,34 @@
 package com.techhounds.commands.bin;
 
+import com.techhounds.subsystems.BinSubsystem;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class SetBin extends Command {
+	
+	private BinSubsystem bin;
+	private int direction;
+	private double power = bin.getPower();
 
-    public SetBin() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+    public SetBin(int direction, double power) {
+    	super("Set Bin");
+    	bin = bin.getInstance();
+    	requires(bin);
+    	this.direction = direction;
+    	this.power = power;
+    }
+    
+    public SetBin(int direction) {
+    	this.direction = direction;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	bin.direction = direction;
+    	bin.power = power;
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -22,7 +37,7 @@ public class SetBin extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
@@ -32,5 +47,6 @@ public class SetBin extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
