2544534a6a0468cce7f4f66cc0aae30e051df40d
claytondetke <cjdet_000@10.35.48.35>
2015-01-26 15:48:30 -0500

    created BinSubsystem

---
+++ src/com/techhounds/subsystems/BinSubsystem.java
@@ -0,0 +1,18 @@
+package com.techhounds.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * @author Alex Fleig, Matt Simons, Ayon Mitra, Clayton Detke, Adam Foster
+ */
+public class BinSubsystem extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
