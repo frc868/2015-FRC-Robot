8c27245d29a0028c990da6da633644fdc3557243
theatifniyaz <theatifniyaz@gmail.com>
2015-01-30 00:35:44 -0500

    Added auton choices

--- src/com/techhounds/commands/auton/AutonChooser.java
+++ src/com/techhounds/commands/auton/AutonChooser.java
@@ -10,23 +10,31 @@ import edu.wpi.first.wpilibj.command.Command;
 public class AutonChooser {
 	
 	public static final String [] AUTON_CHOICES = {
-//		"3 Tote Pickup",
-//		"2 Tote Pickup",
-//		"1 Tote Pickup",
-//		"1 Tote Push",
-//		"Move to Auto Zone",
-		"Do Nothing"};
+		"3 Tote with Bin",
+		"3 Tote",
+		"2 Tote with Bin",
+		"2 Tote",
+		"1 Tote with Bin",
+		"1 Tote",
+		"Bin",
+		"Push",
+		"Move To Auto Zone",
+		"Do Nothing"}; // TODO: Come up with more possibilities. These are the basic.
 
 	public static Command getSelected() {
 		
 		Command [] options = new Command[] {
-//				new DoNothing(),
-//				new DoNothing(),
-//				new DoNothing(),
-//				new DoNothing(),
-//				new DoNothing(),
+				new DoNothing(),
+				new DoNothing(),
+				new DoNothing(),
+				new DoNothing(),
+				new DoNothing(),
+				new DoNothing(),
+				new DoNothing(),
+				new DoNothing(),
+				new DoNothing(),
 				new DoNothing()
-		};
+		}; // TODO: Replace with actual commands
 		
 		return options[OI.getInstance().getAutonChoice()];
 	}
