30410f085b2c251d07c9e9edb3af4c42f2c5a126
Matthew <Matthew@10.35.2.206>
2015-01-29 16:47:28 -0800

    Thing?
    Edited controller map

--- src/com/techhounds/ControllerMap.java
+++ src/com/techhounds/ControllerMap.java
@@ -12,26 +12,27 @@ public class ControllerMap {
 
     public static final int A = 0, B = 1, X = 2, Y = 3, RB = 4, RT = 5, LB = 6,
             LT = 7, START = 8, BACK = 9, LEFT_HORIZONTAL = 10,
-            RIGHT_HORIZONTAL = 11, LEFT_VERTICAL = 12, RIGHT_VERTICAL = 13, 
-            D_PAD_HORIZONTAL = 14, D_PAD_VERTICAL = 15;
+            RIGHT_HORIZONTAL = 11, LEFT_VERTICAL = 12, RIGHT_VERTICAL = 13;
     
-    public static final int RIGHT = 0, LEFT = 1, UP = 2, DOWN = 3; 
+    public static final int RIGHT = 0, LEFT = 1, UP = 2, DOWN = 3, DIAG_UP_RIGHT = 4,
+    		DIAG_UP_LEFT = 5, DIAG_DOWN_RIGHT = 6, DIAG_DOWN_LEFT = 7;
                 
     public static final int LOGITECH = 0, TOMEE = 1, PS3 = 2;
     
     private static String[] buttonName = {"A", "B", "X", "Y", "RB", "RT", "LB",
             "LT", "START", "BACK"};
     
-    private static String[] axeNames = {"LH", "RH", "LV", "RV", "DPAD-H", "DPAD-V"};
+    private static String[] axeNames = {"LH", "RH", "LV", "RV"};
     
     /** TOMEE Cannot get the whole DPad */
-    private static final int[] tomee =      { 3, 2, 4, 1, 8, 6, 7, 5, 10, 9, 1, 3, 2, 5, -1, -1};
+    private static final int[] tomee =      { 3, 2, 4, 1, 8, 6, 7, 5, 10, 9, 1, 3, 2, 5};
     
-    private static final int[] logitech =   { 2, 3, 1, 4, 6, 8, 5, 7, 10, 9, 0, 2, 1, 3, 4, 5};
+    private static final int[] logitech =   { 2, 3, 1, 4, 6, 8, 5, 7, 10, 9, 0, 2, 1, 3};
     
     /** PS3 Cannot get DPAD UP and DOWN) */
-    private static final int[] ps3 =        { 1, 2, 3, 4, 6, 10, 5, 9, 8, 7, 1, 4, 2, 5, 6, -1};
+    private static final int[] ps3 =        { 1, 2, 3, 4, 6, 10, 5, 9, 8, 7, 1, 4, 2, 5};
     
+    private boolean fourDirectional;
     int[] controller;
     private Joystick joystick;
     
@@ -51,6 +52,12 @@ public class ControllerMap {
             controller = tomee;
         else
             controller = logitech;
+        fourDirectional = true;
+    }
+    
+    public ControllerMap(Joystick joystick, int type, boolean isFourDirectional){
+    	this(joystick, type);
+    	fourDirectional = isFourDirectional;
     }
     
     public int index(int id) {
@@ -102,17 +109,39 @@ public class ControllerMap {
         	
         	int angle = joystick.getPOV();
         	
-            if (button == ControllerMap.RIGHT) {
-                return angle == 270 || angle == 315 || angle == 225;
-            } else if (button == ControllerMap.LEFT) {
-                return angle == 90 || angle == 45 || angle== 135;
-            } else if (button == ControllerMap.UP) {
-                return angle == 0 || angle == 45 || angle == 315;
-            } else if (button == ControllerMap.DOWN) {
-                return angle == 180 || angle == 135 || angle == 225;
-            } else {
-                return false;
-            }
+        	if (fourDirectional){
+	            if (button == ControllerMap.RIGHT) {
+	                return angle == 270 || angle == 315 || angle == 225;
+	            } else if (button == ControllerMap.LEFT) {
+	                return angle == 90 || angle == 45 || angle== 135;
+	            } else if (button == ControllerMap.UP) {
+	                return angle == 0 || angle == 45 || angle == 315;
+	            } else if (button == ControllerMap.DOWN) {
+	                return angle == 180 || angle == 135 || angle == 225;
+	            } else {
+	                return false;
+	            }
+        	}else{
+        		if(button == ControllerMap.UP){
+        			return angle == 0;
+        		}else if(button == ControllerMap.DIAG_UP_LEFT){
+        			return angle == 45;
+        		}else if(button == ControllerMap.LEFT){
+        			return angle == 90;
+        		}else if(button == ControllerMap.DIAG_DOWN_LEFT){
+        			return angle == 135;
+        		}else if(button == ControllerMap.DOWN){
+        			return angle == 180;
+        		}else if(button == ControllerMap.DIAG_DOWN_RIGHT){
+        			return angle == 225;
+        		}else if(button == ControllerMap.RIGHT){
+        			return angle == 270;
+        		}else if(button == ControllerMap.DIAG_UP_RIGHT){
+        			return angle == 315;
+        		}else{
+        			return false;
+        		}
+        	}
         }
     }
 }
