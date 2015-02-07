fafce54f871dbe542b8b4f44a22825901537b46a
theatifniyaz <theatifniyaz@gmail.com>
2015-02-07 18:11:51 -0500

    power calculation was wrong

--- src/com/techhounds/commands/auton/DriveToClosestTote.java
+++ src/com/techhounds/commands/auton/DriveToClosestTote.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- *
+ * @author Anonymous :(
  */
 public class DriveToClosestTote extends Command {
 
@@ -15,36 +15,32 @@ public class DriveToClosestTote extends Command {
 	CameraSubsystem cam;
 	
 	private double power = 0;
+	private double offset = 0;
 	
     public DriveToClosestTote() {
     	drive = DriveSubsystem.getInstance();
     	cam = CameraSubsystem.getInstance();
     	requires(drive);
+    	requires(cam);
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
     }
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	power = cam.getDistValue();
-    	double offset = cam.getOffsetValue();
+    	offset = cam.getOffsetValue();
     	
-    	drive.setPower(power - offset, -(power + offset));
+    	drive.setPower(power + offset, -(power - offset));
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return power < .1;
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     	end();
     }
