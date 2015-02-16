5565cc6836a3212cde2457227f12d59b1713e1a9
theatifniyaz <theatifniyaz@gmail.com>
2015-02-15 23:41:27 -0500

    CD says this might work

--- src/com/techhounds/subsystems/USBCameraSubsystem.java
+++ src/com/techhounds/subsystems/USBCameraSubsystem.java
@@ -1,5 +1,8 @@
 package com.techhounds.subsystems;
 
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.Image;
+
 import edu.wpi.first.wpilibj.CameraServer;
 
 public class USBCameraSubsystem extends BasicSubsystem {
@@ -7,10 +10,21 @@ public class USBCameraSubsystem extends BasicSubsystem {
 	private static USBCameraSubsystem instance;
 	private CameraServer server;
 	
+	private int session;
+	private Image frame;
+	
 	private USBCameraSubsystem() {
-		server = CameraServer.getInstance();
-		setQuality(50);
-		startCapture();
+//		server = CameraServer.getInstance();
+//		setQuality(50);
+//		startCapture();
+//		
+		frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+
+        session = NIVision.IMAQdxOpenCamera("cam0",
+                NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+        NIVision.IMAQdxConfigureGrab(session);
+        
+		NIVision.IMAQdxStartAcquisition(session);
 	}
 	
 	public static USBCameraSubsystem getInstance() {
@@ -33,7 +47,8 @@ public class USBCameraSubsystem extends BasicSubsystem {
 
 	@Override
 	public void updateSmartDashboard() {
-		
+		NIVision.IMAQdxGrab(session, frame, 1);
+        CameraServer.getInstance().setImage(frame);
 	}
 
 	@Override
