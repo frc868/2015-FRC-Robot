13d5c195d83385fb2aa2a8d4a9ee6c3c756ca5bd
theatifniyaz <theatifniyaz@gmail.com>
2015-02-19 18:58:20 -0500

    camera stuff should work now

--- src/com/techhounds/subsystems/USBCameraSubsystem.java
+++ src/com/techhounds/subsystems/USBCameraSubsystem.java
@@ -1,8 +1,5 @@
 package com.techhounds.subsystems;
 
-import com.ni.vision.NIVision;
-import com.ni.vision.NIVision.Image;
-
 import edu.wpi.first.wpilibj.CameraServer;
 
 public class USBCameraSubsystem extends BasicSubsystem {
@@ -10,18 +7,9 @@ public class USBCameraSubsystem extends BasicSubsystem {
 	private static USBCameraSubsystem instance;
 	private CameraServer server;
 	
-	private int session;
-	private Image frame;
-	
 	private USBCameraSubsystem() {
-		
-		frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
-
-        session = NIVision.IMAQdxOpenCamera("cam0",
-                NIVision.IMAQdxCameraControlMode.CameraControlModeController);
-        NIVision.IMAQdxConfigureGrab(session);
-        
-        startSession();
+		server = CameraServer.getInstance();
+        startCapture();
 	}
 	
 	public static USBCameraSubsystem getInstance() {
@@ -30,23 +18,18 @@ public class USBCameraSubsystem extends BasicSubsystem {
 		return instance;
 	}
 	
-	private void startSession() {
-		NIVision.IMAQdxStartAcquisition(session);
+	public void setQuality(int quality) {
+		server.setQuality(quality);
 	}
 	
-	public void updateCamera() {
-		
-		NIVision.IMAQdxGrab(session, frame, 1);
-        CameraServer.getInstance().setImage(frame);
+	public void startCapture() {
+		if(!server.isAutoCaptureStarted())
+			server.startAutomaticCapture("cam0");
 	}
 	
-	private void stopSession() {
-		NIVision.IMAQdxStopAcquisition(session);
-	}
-
 	@Override
 	public void updateSmartDashboard() {
-		updateCamera();
+		
 	}
 
 	@Override
